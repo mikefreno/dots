862bbffcc2d48fb3ba2cd1c0283631a1c7955456
Michael Freno <michaelt.freno@gmail.com>
2024-05-15 00:10:06 -0400

    ok i'm done

--- sketchybar/items/mirrors.lua
+++ sketchybar/items/mirrors.lua
@@ -42,18 +42,5 @@ sbar.add("alias", "Cork", {
 	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 'Cork,Item-0'",
 })
 
-sbar.add("alias", "Control Center,Sound", {
-	background = {
-		height = 22,
-		color = colors.bg1,
-		border_color = colors.grey,
-		drawing = true,
-		border_width = 1,
-	},
-	position = "right",
-	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 'Control Center,Sound'",
-})
-
 sbar.exec(string.format("sketchybar --set 'Cork' alias.color=%s", colors.white))
 sbar.exec(string.format("sketchybar --set 'CleanShot X' alias.color=%s", colors.white))
-sbar.exec(string.format("sketchybar --set 'Control Center,Sound' alias.color=%s", colors.white))
