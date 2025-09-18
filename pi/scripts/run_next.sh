52a0180e52973effc9374c67b7fc8ec97195fa5b
Michael Freno <michaelt.freno@gmail.com>
2025-09-18 09:46:17 -0400

    nextcloud corrected

--- pi/scripts/run_next.sh
+++ pi/scripts/run_next.sh
@@ -7,6 +7,7 @@ $DOCKER_PATH run -d \
 --name nextcloud-aio-mastercontainer \
 --restart unless-stopped \
 --publish 8085:8080 \
+--env APACHE_PORT=11000 \
 --env SKIP_DOMAIN_VALIDATION=true \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
