54250144dc40784475f180681b21c321937e7d43
Michael Freno <michaelt.freno@gmail.com>
2025-09-18 09:24:56 -0400

    nextcloud skip domain validation

--- pi/scripts/run_next.sh
+++ pi/scripts/run_next.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 DOCKER_PATH=$(which docker)
-sudo $DOCKER_PATH run \
+$DOCKER_PATH run -d \
 --init \
 --sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
 --restart unless-stopped \
---publish 80:80 \
 --publish 8085:8080 \
+--env SKIP_DOMAIN_VALIDATION=true \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 ghcr.io/nextcloud-releases/all-in-one:latest
