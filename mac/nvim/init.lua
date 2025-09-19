46f669f25f1ad5eadf6da17d1f02207826862bbf
Michael Freno <michaelt.freno@gmail.com>
2025-09-19 10:12:39 -0400

    sync

--- mac/nvim/init.lua
+++ mac/nvim/init.lua
@@ -287,7 +287,7 @@ require("lazy").setup({
 						client
 						and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
 					then
-						map("<leader>th", function()
+						map("<leader>Th", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 						end, "[T]oggle Inlay [H]ints")
 					end
