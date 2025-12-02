ba92b33cf5ab76223a3d569c0f15735da5e4feb5
Michael Freno <michaelt.freno@gmail.com>
2025-12-02 18:20:08 -0500

    adding back mirrors

--- mac/sketchybar/items/mirrors.lua
+++ mac/sketchybar/items/mirrors.lua
@@ -1,15 +1,17 @@
 local colors = require("colors")
 local settings = require("settings")
 
-sbar.add("item", "mirrors.cleanshot.icon", {
+sbar.add("item", "mirrors.controlcenter.icon", {
 	background = {
-		height = 22,
+		height = 24,
 		color = colors.bg1,
 		border_color = colors.grey,
 		border_width = 1,
+		padding_right = 6,
+		padding_left = 0,
 	},
 	icon = {
-		string = "􀐩",
+		string = "􀜊",
 		width = 20,
 		padding_right = 0,
 		padding_left = 5,
@@ -21,27 +23,28 @@ sbar.add("item", "mirrors.cleanshot.icon", {
 		},
 	},
 	position = "right",
-	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 'CleanShot X,Item-0'",
+	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 'Control Center,BentoBox-0'",
 })
-
-sbar.add("item", "mirrors.cork.icon", {
-	background = {
-		height = 22,
-		color = colors.bg1,
-		border_color = colors.grey,
-		border_width = 1,
-	},
-	icon = {
-		string = "􀐯",
-		width = 20,
-		padding_right = 6,
-		padding_left = 5,
-		color = colors.white,
-		font = {
-			style = settings.font.style_map["Regular"],
-			size = 14.0,
-		},
-	},
-	position = "right",
-	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 'Cork,Item-0'",
-})
+-- broken atm due to macos() changes
+--sbar.add("item", "mirrors.betterdisplay.icon", {
+--background = {
+--height = 24,
+--color = colors.bg1,
+--border_color = colors.grey,
+--border_width = 1,
+--},
+--icon = {
+--string = "􀢹",
+--width = 20,
+--padding_right = 0,
+--padding_left = 5,
+--align = "center",
+--color = colors.white,
+--font = {
+--style = settings.font.style_map["Regular"],
+--size = 14.0,
+--},
+--},
+--position = "right",
+--click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s ''",
+--})
