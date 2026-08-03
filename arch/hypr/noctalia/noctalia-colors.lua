694658956835e202e9c21ee95d16bc32eb08bb6e
Mike Freno <michael@freno.me>
2026-08-03 17:47:17 -0400

    hypr: migrate config to Lua (hyprland.lua + noctalia colors as lua)

---
+++ arch/hypr/noctalia/noctalia-colors.lua
@@ -0,0 +1,25 @@
+-- noctalia colors for hyprland.lua
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgb(8ae467)",
+            inactive_border = "rgb(1c2518)",
+        },
+    },
+    group = {
+        col = {
+            border_active        = "rgb(5cd677)",
+            border_inactive      = "rgb(1c2518)",
+            border_locked_active = "rgb(fd4663)",
+            border_locked_inactive = "rgb(1c2518)",
+        },
+        groupbar = {
+            col = {
+                active             = "rgb(5cd677)",
+                inactive           = "rgb(1c2518)",
+                locked_active      = "rgb(fd4663)",
+                locked_inactive    = "rgb(1c2518)",
+            },
+        },
+    },
+})
